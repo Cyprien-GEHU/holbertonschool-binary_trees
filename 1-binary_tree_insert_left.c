14559440166fa62a8d7aaac23d9e2950845597c0
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-09 13:14:19 +0100

    created 1-binary_tree_insert_left.c

---
+++ 1-binary_tree_insert_left.c
@@ -0,0 +1,26 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_insert_left - Inserts a node as the left-child of another node.
+ * @parent: Parent of the node.
+ * @value: Value of the node.
+ *
+ * Return: Pointer to the created node or NULL if failure.
+ */
+
+binary_tree_t	*binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *new_node;
+
+	new_node = binary_tree_node(NULL, value);
+	if (parent == NULL || new_node == NULL)
+		return (NULL);
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+		new_node->left = parent->left;
+	}
+	parent->left = new_node;
+	new_node->parent = parent;
+	return (new_node);
+}
