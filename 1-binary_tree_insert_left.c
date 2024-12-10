e5aa59de6dbed7207bd00b8dee86b139931b21ad
SoliraZ <48773711+SoliraZ@users.noreply.github.com>
2024-12-10 15:17:59 +0100

    added the first condition on top of the function

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,8 +12,10 @@ binary_tree_t	*binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
+	if (parent == NULL)
+		return (NULL);
 	new_node = binary_tree_node(NULL, value);
-	if (parent == NULL || new_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 	if (parent->left != NULL)
 	{
